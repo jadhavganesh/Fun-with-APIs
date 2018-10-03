eb3555735dbdcff1c46c624031e317d85325c1ff
jadhavganesh <jadhavganesh0301@gmail.com>
2018-10-03 12:20:06 +0530

    New Functions add

--- api.py
+++ api.py
@@ -30,4 +30,18 @@ def home():
 @app.route('/api/v1/resources/books/all',methods=['GET'])
 def api_all():
 	return jsonify(books)
+
+
+@app.route('/api/v1/resources/books',methods=['GET'])
+def api_id():
+	if 'id' in request.args:
+		id=int(request.args['id'])
+	else:
+		return "Error: No id field provide. Please specify an id."
+	results=[]
+
+	for book in books:
+		if book['id']==id:
+			results.append(book)
+	return jsonify(results)
 app.run()
